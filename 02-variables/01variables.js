a4e1a2318ed1d16808763192ada6daa1acff2b51
Alfonso Tienda <atienda@iprocuratio.com>
2020-03-20 11:15:24 +0100

    inicial

---
+++ 02-variables/01variables.js
@@ -0,0 +1,16 @@
+var myFirstVariable; // undefined
+document.write(myFirstVariable);
+
+document.write("<br/>");
+
+// = es el operador de asignación
+var miNombre = "Alfonso";
+document.write(miNombre);
+document.write("<br/>");
+
+// Varias inicializaciones en una misma linea
+var miApellido = "Tienda", miProfesion = "Informática";
+document.write(miApellido);
+document.write("<br/>");
+document.write(miProfesion);
+document.write("<br/>");
---
+++ 02-variables/02operaciones.js
@@ -0,0 +1,16 @@
+
+
+// A partir de ahora usaremos let por defecto, en lugar de var
+let a = 10, b = 5;
+
+let suma = a+b;
+
+document.write("Suma = " + suma + "<br/>");
+
+let resta = a-b;
+
+document.write("Resta = " + resta + "<br/>");
+
+let division = a/b;
+
+document.write ("Division ")
